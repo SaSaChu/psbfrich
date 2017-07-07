6a0ca8a17fe9064e3509a576cf49c7c2a1e2af0d
SaSaChu <teresa7397@gmail.com>
2017-07-07 23:36:22 +0800

    首頁完成

--- js/public.js
+++ js/public.js
@@ -13,9 +13,17 @@ $(function () {
     $('#nav-icon3').click(function(){
       $(this).toggleClass('open');
 
-      $('#menu_boxs').toggleClass('s_menu');
+      $('#r_menu_boxs').toggleClass('s_menu');
       $('#menu_back').toggleClass('s_menu_b');
     });
+  });
+
+
+  $('.se_menu').click(function() {
+    $(this).toggleClass('s_open');
+    $('.se_div_box').toggleClass('show_div');
+
+    $('.se_menu').eq (0).click();
   });
 
 
